a6bf14e3ce45a0e5de75c7619e32bfb03142bf59
Joe Bernitt <j.b5286@gmail.com>
2019-12-20 18:45:56 -0500

    Updates

---
+++ precache-manifest.c96e0508b79cc18e1b453e5505b6040c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a05dfcc8bc56175305dcc365eac70e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0526c18f9f986f9002a",
+    "url": "/static/css/main.9729c765.chunk.css"
+  },
+  {
+    "revision": "188a51d734d9544dd76a",
+    "url": "/static/js/2.9279dd4e.chunk.js"
+  },
+  {
+    "revision": "421bd247d27199660986f750cdf8b760",
+    "url": "/static/js/2.9279dd4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0526c18f9f986f9002a",
+    "url": "/static/js/main.81c53c03.chunk.js"
+  },
+  {
+    "revision": "c22a624ec1bd8e337a3b",
+    "url": "/static/js/runtime-main.a08854ca.js"
+  },
+  {
+    "revision": "cf958035506798ddc0ee2b2de993a535",
+    "url": "/static/media/hero-bg.cf958035.jpg"
+  },
+  {
+    "revision": "df2a974245e27333f027fd7c660569e7",
+    "url": "/static/media/hero-sidebar-bg.df2a9742.png"
+  },
+  {
+    "revision": "614c8389c8b69b32a32028bc5dff3b1b",
+    "url": "/static/media/metamask-icon.614c8389.png"
+  },
+  {
+    "revision": "460f3b1a7a75ff9af795596902fb9dea",
+    "url": "/static/media/squares-logo-mock.460f3b1a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31f952e95800ed858574f59e288a0025.js"
+  "/precache-manifest.c96e0508b79cc18e1b453e5505b6040c.js"
 );
 
 self.addEventListener('message', (event) => {
