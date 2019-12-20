0db269947e86b1dc63f8b819fcb3c95e01f5deb8
Joe Bernitt <j.b5286@gmail.com>
2019-12-20 14:02:26 -0500

    Updates

---
+++ precache-manifest.50bfc92a9c4bf7c704ecf2754a97652e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e0edd03254fbcf52617644e8d627ba",
+    "url": "/football-squares-dapp/index.html"
+  },
+  {
+    "revision": "b53e40f457fc4b3e5ec0",
+    "url": "/football-squares-dapp/static/css/main.6a7fdf9d.chunk.css"
+  },
+  {
+    "revision": "c40c039442b2a3abda68",
+    "url": "/football-squares-dapp/static/js/2.80110cb7.chunk.js"
+  },
+  {
+    "revision": "421bd247d27199660986f750cdf8b760",
+    "url": "/football-squares-dapp/static/js/2.80110cb7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b53e40f457fc4b3e5ec0",
+    "url": "/football-squares-dapp/static/js/main.9406f70b.chunk.js"
+  },
+  {
+    "revision": "c6273bf5dd713fa81e56",
+    "url": "/football-squares-dapp/static/js/runtime-main.8ec88141.js"
+  },
+  {
+    "revision": "cf958035506798ddc0ee2b2de993a535",
+    "url": "/football-squares-dapp/static/media/hero-bg.cf958035.jpg"
+  },
+  {
+    "revision": "460f3b1a7a75ff9af795596902fb9dea",
+    "url": "/football-squares-dapp/static/media/squares-logo-mock.460f3b1a.png"
+  }
+]);
