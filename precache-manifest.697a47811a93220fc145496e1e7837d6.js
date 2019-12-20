a790050ea177633546a7e9f308766bf0f9c1aac3
Joe Bernitt <j.b5286@gmail.com>
2019-12-20 16:00:13 -0500

    Updates

---
+++ precache-manifest.697a47811a93220fc145496e1e7837d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105fa0815d8f29b840c562898ba564f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "469588556994c708f40c",
+    "url": "/static/css/main.2d77f1d1.chunk.css"
+  },
+  {
+    "revision": "fac6ed4ec9ba6665cb18",
+    "url": "/static/js/2.7316b40f.chunk.js"
+  },
+  {
+    "revision": "421bd247d27199660986f750cdf8b760",
+    "url": "/static/js/2.7316b40f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "469588556994c708f40c",
+    "url": "/static/js/main.5a04bde6.chunk.js"
+  },
+  {
+    "revision": "c22a624ec1bd8e337a3b",
+    "url": "/static/js/runtime-main.a08854ca.js"
+  },
+  {
+    "revision": "cf958035506798ddc0ee2b2de993a535",
+    "url": "/static/media/hero-bg.cf958035.jpg"
+  },
+  {
+    "revision": "460f3b1a7a75ff9af795596902fb9dea",
+    "url": "/static/media/squares-logo-mock.460f3b1a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/football-squares-dapp/precache-manifest.4d730fdc0d6a5da43659da261ad545a4.js"
+  "/precache-manifest.697a47811a93220fc145496e1e7837d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/football-squares-dapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
